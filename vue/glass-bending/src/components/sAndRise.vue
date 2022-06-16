0694846a542e37cbda7ba26d5b461e0592c6d1af
Tr0f1k <84199789+Tr0f1k@users.noreply.github.com>
2022-06-16 14:49:01 +0300

    Add files via upload

---
+++ vue/glass-bending/src/components/sAndRise.vue
@@ -0,0 +1,53 @@
+<template>
+    <table id="table1">
+                <tr>
+                    <td>
+                        Height:
+                    </td>
+                    <td>
+                        <input id="height1" size="1">
+                    </td>
+                    <td>
+                        mm
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Thickness:
+                    </td>
+                    <td>
+                        <input id="thickness1" size="1">
+                    </td>
+                    <td>
+                        mm
+                    </td>
+                </tr>
+                <tr>
+                <td>
+                    Span:
+                </td>
+                <td style="width: 10%">
+                    <input id="1" size="1">
+                </td>
+                <td>
+                    mm
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Rise:
+                </td>
+                <td style="width: 10%">
+                    <input id="2" size="1">
+                </td>
+                <td>
+                    mm
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <button id="button1" onclick="spanrise()">Calculate!</button>
+                </td>
+            </tr>
+        </table>
+</template>
